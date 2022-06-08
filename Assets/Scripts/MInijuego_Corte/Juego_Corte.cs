0c89f86da3826a6caa85238e7e9e32936468d973
sebastian <sebastiansaavedra4515@gmail.com>
2022-06-07 20:56:10 -0500

    s
    s

---
+++ Assets/Scripts/MInijuego_Corte/Juego_Corte.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Juego_Corte : MonoBehaviour
+{
+    public Text timeText;
+    public float Timemax=30;
+    float time=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+        timeText.text = "" +(int)(Timemax-time);
+        //if()
+    }
+}
